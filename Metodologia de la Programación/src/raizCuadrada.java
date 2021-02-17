7e6f24cf4d1f421ae691cb5c8bf8146f8936f529
Alberto Vazquez <albertovmrm@gmail.com>
2021-02-17 19:50:09 +0100

    Time arrays

--- Metodologia de la Programación/src/raizCuadrada.java
+++ Metodologia de la Programación/src/raizCuadrada.java
@@ -3,15 +3,23 @@ public class raizCuadrada {
 	public static void main(String[] args) {
 		
 		int ns [] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225};
-
-		raizCuadradaMath(ns);
-		metodoBabilonico(ns);
+		
+		long time1[] = new long[ns.length];
+		long time2[] = new long[ns.length];
+		long time3[] = new long[ns.length];
+		long time4[] = new long[ns.length];
+		
+		raizCuadradaMath(ns, time1);
+		metodoBabilonico(ns, time2);
 		busquedaBinaria();
 		raizCuadradaRecursiva();
 		
+		for (int i = 0; i < time1.length; i++) {
+			System.out.println("Tiempo numero: " + ns[i] + " / math: " + time1[i] + "s / bab:  " + time2[i] + "s");
+		}
 	}
 	
-	public static void raizCuadradaMath(int numbers[]) {
+	public static void raizCuadradaMath(int numbers[], long time[]) {
 		long t1, t0;
 		double t = 0;
 		
@@ -20,14 +28,16 @@ public class raizCuadrada {
 			t0 = System.nanoTime();
 			System.out.println("Raíz cuadrada de " + numbers[i] + " = " + Math.sqrt(numbers[i]));
 			t1 = System.nanoTime();
+			time[i] = t1-t0;
 			t += (t1 - t0)/1e9;
+			
 		}	
 		
 		System.out.println("\nTiempo de ejecución: " + (t/numbers.length) + " segundos");
 	}
 	
 	
-	public static void metodoBabilonico(int numbers[]) {
+	public static void metodoBabilonico(int numbers[], long time[]) {
 		System.out.println("\nMétodo babilonico: ");
 		
 		long t1, t0 = 0;
@@ -54,6 +64,7 @@ public class raizCuadrada {
 				}
 			}
 			t1 = System.nanoTime();
+			time[i] = t1-t0;
 			t += (t1 - t0)/1e9;
 		}
 		
