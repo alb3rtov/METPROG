a8924818c3583f7b424f32b5b58906d29cdda867
Alberto Vazquez <albertovmrm@gmail.com>
2021-02-17 18:13:58 +0100

    Add babylonian method

--- Metodologia de la Programación/src/raizCuadrada.java
+++ Metodologia de la Programación/src/raizCuadrada.java
@@ -5,7 +5,7 @@ public class raizCuadrada {
 		int ns [] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225};
 
 		raizCuadradaMath(ns);
-		metodoBabilonio();
+		metodoBabilonico(ns);
 		busquedaBinaria();
 		raizCuadradaRecursiva();
 		
@@ -14,7 +14,7 @@ public class raizCuadrada {
 	public static void raizCuadradaMath(int numbers[]) {
 		long t1, t0;
 		double t = 0;
-
+		
 		System.out.println("Método math: ");
 		for (int i = 0; i < numbers.length; i++) {
 			t0 = System.nanoTime();
@@ -27,9 +27,39 @@ public class raizCuadrada {
 	}
 	
 	
-	public static void metodoBabilonio() {
+	public static void metodoBabilonico(int numbers[]) {
+		System.out.println("\nMétodo babilonico: ");
+		
+		long t1, t0 = 0;
+		double t = 0;
+		double n = 0, nn = 0, r;
+		double min = Double.MAX_VALUE;
+		double aux, m_babilonico;
 		
 		
+		for (int i = 0; i < numbers.length; i++) {
+			t0 = System.nanoTime();
+			for (int j = 0; j < Integer.MAX_VALUE; j++) {
+				r = j * j;
+				aux = Math.abs(r - numbers[i]);
+				if (aux < min) {
+					n = j;
+					nn = r;
+					min = aux;
+				} else {
+					m_babilonico = (numbers[i] + nn) / (2 * n);
+					System.out.println("Raiz cuadrada de " + numbers[i] + "= " + m_babilonico);
+					min = Double.MAX_VALUE;
+					break;
+				}
+			}
+			t1 = System.nanoTime();
+			t += (t1 - t0)/1e9;
+		}
+		
+
+		
+		System.out.println("\nTiempo de ejecución: " + t + " segundos");
 	}
 	
 	public static void busquedaBinaria() {
