3c9851975df997bc8bfa3db0339418d28bf867c3
Alberto Vazquez <albertovmrm@gmail.com>
2021-02-27 11:47:36 +0100

    Print times and quit prints sqrts results

--- Metodologia de la Programación/src/P1/raizCuadrada.java
+++ Metodologia de la Programación/src/P1/raizCuadrada.java
@@ -2,35 +2,75 @@
  * Class Name: raizCuadrada
  * <p>
  * Clase que calcula de 4 formas diferentes la raíz cuadrada
- * de un array de numeros enteros definido.
+ * de un array de numeros enteros.
  * </p>
  * @author Alberto Vázquez Martínez y Ángel Villafranca Iniesta
  */
 
 package P1;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class raizCuadrada {
 	
 	final static int SQUARE = 2;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
-		int ns [] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225};
+		//int ns [] = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225};
+		//int ns [] = {400,504,675,876,987,1023,2321,3542,4353,5646,6040,8004,10320,12340};
+		ArrayList<Float> ns = new ArrayList<>();
+
+		File f=new File("numbers.txt");
+		Scanner filename = new Scanner (f);
 		
-		long time1[] = new long[ns.length];
-		long time2[] = new long[ns.length];
-		long time3[] = new long[ns.length];
-		long time4[] = new long[ns.length];
+		while (filename.hasNext()){
+			ns.add(filename.nextFloat());
+		}
 		
-		raizCuadradaMath(ns, time1);
-		metodoBabilonico(ns, time2);
-		busquedaBinaria(ns, time3);
-		raizCuadradaRecursiva(ns, time4);
+		filename.close();
 		
-		/*
-		for (int i = 0; i < time1.length; i++) {
-			System.out.println("Tiempo numero: " + ns[i] + " / math: " + time1[i] + "s / bab:  " + time2[i] + "s");
-		}*/
+		double sqrt1[] = new double[ns.size()];
+		double sqrt2[] = new double[ns.size()];
+		double sqrt3[] = new double[ns.size()];
+		double sqrt4[] = new double[ns.size()];
+		
+		long time1[] = new long[ns.size()];
+		long time2[] = new long[ns.size()];
+		long time3[] = new long[ns.size()];
+		long time4[] = new long[ns.size()];
+		
+		sqrtMath(ns, sqrt1, time1);
+		sqrtBabilonico(ns, sqrt2, time2);
+		sqrtBinarySearch(ns, sqrt3, time3);
+		sqrtRecursive(ns, sqrt4, time4);
+		
+		printTimeResults(ns, sqrt1, sqrt2, sqrt3, sqrt4, time1, time2, time3, time4);
+	}
+	
+	/**
+	 * <p> Imprime los resultados de tiempo respecto a los numeros de la lista.
+	 * </p>
+	 * @param numbers
+	 * @param sqrt1
+	 * @param sqrt2
+	 * @param sqrt3
+	 * @param sqrt4
+	 * @param time1
+	 * @param time2
+	 * @param time3
+	 * @param time4
+	 */
+	public static void printTimeResults(ArrayList<Float> numbers, double sqrt1[], double sqrt2[], 
+				double sqrt3[], double sqrt4[], long time1[], long time2[], long time3[], long time4[]) {
+		
+		System.out.printf("\n%-10s |   %-10s |   %-15s |   %-15s |   %-10s\n","Number","Math (ns)","Babilonico (ns)","Binary Search (ns)","Recursive (ns)");
+		System.out.println("-----------|--------------|-------------------|----------------------|-----------------");
+		for (int i = 0; i < numbers.size(); i++) {
+			System.out.printf("%-10s |   %-10s |   %-15s |   %-15s    |   %-10s\n",numbers.get(i), time1[i], time2[i], time3[i], time4[i]);
+		}
 	}
 	
 	/**
@@ -39,21 +79,22 @@ public class raizCuadrada {
 	 * @param numbers Array de números
 	 * @param time Array con los tiempos de ejecución
 	 */
-	public static void raizCuadradaMath(int numbers[], long time[]) {
+	public static void sqrtMath(ArrayList<Float> numbers, double sqrt[], long time[]) {
 		long t1, t0;
 		double t = 0;
+
+		System.out.println("Método biblioteca Math:");
 		
-		System.out.println("Método biblioteca Math:\n");
-		
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < numbers.size(); i++) {
 			t0 = System.nanoTime();
-			System.out.println("Raíz cuadrada de " + numbers[i] + " = " + Math.sqrt(numbers[i]));
+			//System.out.println("Raíz cuadrada de " + numbers.get(i) + " = " + Math.sqrt(numbers.get(i)));
+			sqrt[i] = Math.sqrt(numbers.get(i));
 			t1 = System.nanoTime();
 			time[i] = t1-t0;
 			t += (t1 - t0)/1e9;
 		}	
 		
-		System.out.println("\nTiempo de ejecución: " + (t/numbers.length) + " segundos");
+		System.out.println("Tiempo de ejecución: " + (t/numbers.size()) + " segundos");
 	}
 	
 	/**
@@ -62,29 +103,29 @@ public class raizCuadrada {
 	 * @param numbers Array de números
 	 * @param time Array con los tiempos de ejecución
 	 */
-	public static void metodoBabilonico(int numbers[], long time[]) {		
+	public static void sqrtBabilonico(ArrayList<Float> numbers, double sqrt[], long time[]) {		
 		long t1, t0 = 0;
 		double t = 0;
 		double n = 0, nn = 0, r;
 		double min = Double.MAX_VALUE;
-		double aux, m_babilonico;
+		double aux;
 		
-		System.out.println("\nMétodo babilonico:\n");
+		System.out.println("\nMétodo babilonico:");
 		
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < numbers.size(); i++) {
 			t0 = System.nanoTime();
 			
 			for (int j = 0; j < Integer.MAX_VALUE; j++) {
 				r = j * j;
-				aux = Math.abs(r - numbers[i]);
+				aux = Math.abs(r - numbers.get(i));
 				
 				if (aux < min) {
 					n = j;
 					nn = r;
 					min = aux;
 				} else {
-					m_babilonico = (numbers[i] + nn) / (2 * n);
-					System.out.println("Raiz cuadrada de " + numbers[i] + "= " + m_babilonico);
+					sqrt[i] = (numbers.get(i) + nn) / (2 * n);
+					//System.out.println("Raiz cuadrada de " + numbers.get(i) + "= " + m_babilonico);
 					min = Double.MAX_VALUE;
 					break;
 				}
@@ -95,7 +136,7 @@ public class raizCuadrada {
 			t += (t1 - t0)/1e9;
 		}
 
-		System.out.println("\nTiempo de ejecución: " + (t/numbers.length) + " segundos");
+		System.out.println("Tiempo de ejecución: " + (t/numbers.size()) + " segundos");
 	}
 
 	/**
@@ -104,21 +145,21 @@ public class raizCuadrada {
 	 * @param numbers Array de números
 	 * @param time Array con los tiempos de ejecución
 	 */
-	public static void busquedaBinaria(int numbers[], long time[]) {
+	public static void sqrtBinarySearch(ArrayList<Float> numbers, double sqrt[], long time[]) {
 		long t1, t0;
 		double t = 0;
+		System.out.println("\nMétodo busqueda binaria:");
 		
-		System.out.println("\nMétodo busqueda binaria:\n");
-		
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < numbers.size(); i++) {
 			t0 = System.nanoTime();
-			System.out.println("Raíz cuadrada de " + numbers[i] + "= " + sqrtBS(numbers[i],numbers[i]/2));
+			//System.out.println("Raíz cuadrada de " + numbers.get(i) + "= " + sqrtBS(numbers.get(i),numbers.get(i)/2));
+			sqrt[i] = sqrtBS(numbers.get(i),numbers.get(i)/2);
 			t1 = System.nanoTime();
 			time[i] = t1-t0;
 			t += (t1 - t0)/1e9;
 		}
 		
-		System.out.println("\nTiempo de ejecución " + (t/numbers.length) + " segundos");
+		System.out.println("Tiempo de ejecución " + (t/numbers.size()) + " segundos");
 	}
 	
 	/**
@@ -128,9 +169,9 @@ public class raizCuadrada {
 	 * @param time Array con los tiempos de ejecución
 	 * @return Raiz cuadrada del número dado
 	 */
-	public static float sqrtBS(int number, int precision) { 
-        int start = 0, end = number; 
-        int mid;  
+	public static float sqrtBS(float number, float precision) { 
+        float start = 0, end = number; 
+        float mid;  
         double ans = 0;
         double increment = 0.01;
         
@@ -162,25 +203,27 @@ public class raizCuadrada {
 	
 	/**
 	 * <p>
+	 * Bucle que recorre el array de numeros llama a root para calcula la raiz cuadrada
 	 * </p>
 	 * @param numbers Array de números
 	 * @param time Array con los tiempos de ejecución
 	 */
-	public static void raizCuadradaRecursiva(int numbers[], long time[]) {
+	public static void sqrtRecursive(ArrayList<Float> numbers, double sqrt[], long time[]) {
 		long t1, t0;
 		double t = 0;
 		
-		System.out.println("\nMétodo recursivo:\n");
+		System.out.println("\nMétodo recursivo:");
 		
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < numbers.size(); i++) {
 			t0 = System.nanoTime();
-			System.out.printf("Raíz cuadrada de %d = %.2f\n", numbers[i], root(numbers[i],SQUARE));
+			//System.out.printf("Raíz cuadrada de %.1f = %.2f\n", numbers.get(i), root(numbers.get(i),SQUARE));
+			sqrt[i] = root(numbers.get(i), SQUARE);
 			t1 = System.nanoTime();
 			time[i] = t1-t0;
 			t += (t1 - t0)/1e9;
 		}
 		
-		System.out.println("\nTiempo de ejecución " + (t/numbers.length) + " segundos");
+		System.out.println("Tiempo de ejecución " + (t/numbers.size()) + " segundos");
 	}
 	
 	/**
